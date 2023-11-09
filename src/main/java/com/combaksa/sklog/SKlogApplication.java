09e40aeb668993e6a248d4bd9bc8b899fc5e0655
DESKTOP-BJ8G1LU\nikph <2020113392@dgu.ac.kr>
2023-11-09 20:06:39 +0900

    chore: init

---
+++ src/main/java/com/combaksa/sklog/SKlogApplication.java
@@ -0,0 +1,13 @@
+package com.combaksa.sklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SKlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SKlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/combaksa/sklog/SKlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.combaksa.sklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SKlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
