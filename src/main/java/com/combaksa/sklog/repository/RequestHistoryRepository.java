5223e4fb34e9c6625c0abc0deb122330ab8aacef
jangwoojin1 <jangwoojin1@gmail.com>
2023-11-18 00:11:50 +0900

    Feat: Implement code for storing and retrieving UserRequestHistory in DB

--- src/main/java/com/combaksa/sklog/repository/RequestHistoryRepository.java
+++ src/main/java/com/combaksa/sklog/repository/RequestHistoryRepository.java
@@ -4,9 +4,10 @@ import com.combaksa.sklog.domain.RequestHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RequestHistoryRepository extends JpaRepository<RequestHistory, Long> {
-    Optional<RequestHistory> findByUserContent(String userContent);
+    List<RequestHistory> findAllByRequest(String request);
 }
